babad746d191b6a6a8c76f09af107bfb51a31f78
Javohir Khamidullaev <97449821+Javohir-dev@users.noreply.github.com>
2023-08-07 19:07:23 +0500

    LogIn

--- website/views.py
+++ website/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, "home.html", {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been successfuly logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, please try again!")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
+        
+
+
+
+def logout_user(request):
+    ...
